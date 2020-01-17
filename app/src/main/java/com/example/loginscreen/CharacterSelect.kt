c9889ae76f213ef6914cdf0a0b5de3af8e288876
Elyprix <curtisliudragon@gmail.com>
2020-01-17 15:41:32 -0500

    New Character Screen

---
+++ app/src/main/java/com/example/loginscreen/CharacterSelect.kt
@@ -0,0 +1,12 @@
+package com.example.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterSelect : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_select)
+    }
+}
